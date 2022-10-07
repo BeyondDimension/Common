15c0f5eb713aad548e15f1a44cce8a8ace61d3c7
Aigio Liu <lysoft.pw@foxmail.com>
2022-10-07 18:27:34 +0800

    fix db

--- src/BD.Common.AspNetCore.Identity.BackManage/Data/BackManageDbContextBase.cs
+++ src/BD.Common.AspNetCore.Identity.BackManage/Data/BackManageDbContextBase.cs
@@ -6,7 +6,7 @@ namespace BD.Common.Data;
 public partial class BackManageDbContextBase : IdentityDbContext<BMUser, IdentityRole<Guid>, Guid>, IApplicationDbContext<BMUser>
 {
 #pragma warning disable CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑声明为可以为 null。
-    public BackManageDbContextBase(DbContextOptions<BackManageDbContextBase> options) : base(options)
+    public BackManageDbContextBase(DbContextOptions options) : base(options)
 #pragma warning restore CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑声明为可以为 null。
     {
 
