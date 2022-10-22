f4a8a5cf77199dc26949bc4679fffb30f3c473ac
Mossimo <xflat7@hotmail.com>
2022-10-22 19:53:51 +0800

    misc

--- src/BD.Common.AspNetCore.Identity.BackManage/Entities/Authority/MenuButton.cs
+++ src/BD.Common.AspNetCore.Identity.BackManage/Entities/Authority/MenuButton.cs
@@ -2,11 +2,10 @@ namespace BD.Common.Entities;
 
 public class MenuButton : Entity<Guid>, IOrder
 {
-
     /// <summary>
     /// 租户
     /// </summary>
-    public Guid RoleId { get; set; }
+    public Guid TenanId { get; set; }
 
     public Guid MenuId { get; set; }
 
