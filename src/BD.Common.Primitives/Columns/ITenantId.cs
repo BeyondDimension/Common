101ef9bd94e79843a4adbea779e1a939b5fabbfe
Aigio Liu <lysoft.pw@foxmail.com>
2022-10-23 23:56:06 +0800

    Misc

--- src/BD.Common.Primitives/Columns/ITenantId.cs
+++ src/BD.Common.Primitives/Columns/ITenantId.cs
@@ -1,13 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+namespace BD.Common.Columns;
 
-namespace BD.Common.Columns
+public interface ITenant
 {
-    public interface ITenant
-    {
-        Guid TenantId { get; set; }
-    }
+    Guid TenantId { get; set; }
 }
