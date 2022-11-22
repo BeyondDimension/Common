5eb8845dd3c7182fc2c40d476fc908fb4be2e490
Aigio Liu <lysoft.pw@foxmail.com>
2022-11-22 21:47:42 +0800

    Misc

---
+++ src/BD.Common.Navigation/Properties/ThisAssembly.AssemblyProduct.cs
@@ -0,0 +1,14 @@
+using _ThisAssembly_ = BD.Common.Navigation.ThisAssembly;
+
+[assembly: AssemblyProduct(_ThisAssembly_.AssemblyProduct)]
+
+// ReSharper disable once CheckNamespace
+namespace BD.Common.Navigation;
+
+static partial class ThisAssembly
+{
+    /// <summary>
+    /// 定义程序集清单的产品名自定义属性
+    /// </summary>
+    public const string AssemblyProduct = "次元超越导航库";
+}
---
+++ src/BD.Common.Navigation/Services/INavigationService.cs
@@ -0,0 +1,19 @@
+namespace BD.Common.Services;
+
+/// <summary>
+/// 页面跳转导航服务
+/// </summary>
+public interface INavigationService
+{
+    /// <summary>
+    /// 页面跳转
+    /// </summary>
+    /// <param name="type"></param>
+    /// <param name="flags"></param>
+    void Push(Type type, PushFlags flags = PushFlags.Empty);
+
+    /// <summary>
+    /// 后退
+    /// </summary>
+    void Pop();
+}
