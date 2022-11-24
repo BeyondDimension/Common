03b5bff1a3fddf7391715f3942e8d8b4c156f796
Aigio Liu <lysoft.pw@foxmail.com>
2022-11-24 17:53:21 +0800

    GeneratedRegex

--- src/BD.Common/Extensions/StringExtensions.IsEmail.cs
+++ src/BD.Common/Extensions/StringExtensions.IsEmail.cs
@@ -1,5 +1,4 @@
 using System.Text.RegularExpressions;
-using static System.Text.RegularExpressions.Regexps;
 
 // ReSharper disable once CheckNamespace
 namespace System;
@@ -13,5 +12,13 @@ public static partial class StringExtensions
     /// </summary>
     /// <param name="str">The string to test.</param>
     /// <returns><c>true</c> for a valid email address; otherwise, <c>false</c>.</returns>
-    public static bool IsEmail(this string str) => Regex.IsMatch(str, Email);
+    public static bool IsEmail(this string str) => EmailRegex().IsMatch(str);
+
+    /// <summary>
+    /// Regular expression for matching an email address.
+    /// </summary>
+    /// <remarks>General Email Regex (RFC 5322 Official Standard) from https://emailregex.com.</remarks>
+    /// <returns></returns>
+    [GeneratedRegex("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")]
+    private static partial Regex EmailRegex();
 }
