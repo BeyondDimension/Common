510c0a71923230314871b08498ac70ee611ff1cc
Aigio Liu <lysoft.pw@foxmail.com>
2023-03-07 11:17:25 +0800

    Misc

--- src/BD.Common.Primitives/Columns/MaxLengths.cs
+++ src/BD.Common.Primitives/Columns/MaxLengths.cs
@@ -19,17 +19,65 @@ public static class MaxLengths
     /// </summary>
     public const int SMS_CAPTCHA = 6;
 
+    /// <summary>
+    /// 用户名(用户名不是昵称，通常为唯一键，因可用用户名进行登录)
+    /// </summary>
     public const int UserName = 128;
+
     public const int Url = 2048;
+
+    /// <summary>
+    /// 一般文本字符串
+    /// </summary>
     public const int Text = 1000;
+
+    /// <summary>
+    /// 文件扩展名，例如 .exe/.dll
+    /// </summary>
     public const int FileExtension = 16;
+
+    /// <summary>
+    /// 微信 OpenId
+    /// </summary>
     public const int WeChatId = 128;
+
+    /// <summary>
+    /// 微信 UnionId
+    /// </summary>
     public const int WeChatUnionId = 192;
+
+    /// <summary>
+    /// 邮箱地址
+    /// </summary>
     public const int Email = 256;
+
+    /// <summary>
+    /// 禁用原因
+    /// </summary>
     public const int DisableReason = 1000;
+
+    /// <summary>
+    /// 备注
+    /// </summary>
     public const int Remarks = 1000;
+
+    /// <summary>
+    /// 现实地址/收货地址
+    /// </summary>
     public const int RealityAddress = 150;
+
+    /// <summary>
+    /// 标题
+    /// </summary>
     public const int Title = 30;
+
+    /// <summary>
+    /// 长标题
+    /// </summary>
     public const int LongTitle = 200;
+
+    /// <summary>
+    /// 描述
+    /// </summary>
     public const int Describe = 500;
 }
