49bfc89f569fb8229bd849b126c5497cc1a2c082
Aigio Liu <lysoft.pw@foxmail.com>
2023-05-10 17:32:21 +0800

    🔖 1.23.10510.11732

---
+++ src/BD.Common/Threading/Tasks/Task2.cs
@@ -0,0 +1,69 @@
+namespace System.Threading.Tasks;
+
+public static class Task2
+{
+    public static async void InBackground(Action action, bool longRunning = false)
+    {
+        ArgumentNullException.ThrowIfNull(action);
+
+        TaskCreationOptions options = TaskCreationOptions.DenyChildAttach;
+
+        if (longRunning)
+        {
+            options |= TaskCreationOptions.LongRunning | TaskCreationOptions.PreferFairness;
+        }
+
+        await Task.Factory.StartNew(action, CancellationToken.None, options, TaskScheduler.Default).ConfigureAwait(false);
+    }
+
+    public static void InBackground<T>(Func<T> function, bool longRunning = false)
+    {
+        ArgumentNullException.ThrowIfNull(function);
+
+        InBackground(void () => function(), longRunning);
+    }
+
+    public static async Task<IList<T>> InParallel<T>(
+        IEnumerable<Task<T>> tasks,
+        bool minMemoryUsage = false)
+    {
+        ArgumentNullException.ThrowIfNull(tasks);
+
+        IList<T> results;
+
+        if (minMemoryUsage)
+        {
+            results = new List<T>();
+
+            foreach (Task<T> task in tasks)
+            {
+                results.Add(await task.ConfigureAwait(false));
+            }
+        }
+        else
+        {
+            results = await Task.WhenAll(tasks).ConfigureAwait(false);
+        }
+
+        return results;
+    }
+
+    public static async Task InParallel(
+        IEnumerable<Task> tasks,
+        bool minMemoryUsage = false)
+    {
+        ArgumentNullException.ThrowIfNull(tasks);
+
+        if (minMemoryUsage)
+        {
+            foreach (Task task in tasks)
+            {
+                await task.ConfigureAwait(false);
+            }
+        }
+        else
+        {
+            await Task.WhenAll(tasks).ConfigureAwait(false);
+        }
+    }
+}
