f0fa19171b0705bbf23aef1b49baaf9e74437467
Aigio Liu <lysoft.pw@foxmail.com>
2023-05-18 19:45:34 +0800

    ✨ 新的 ImageHttpClientService 实现，使用 Fusillade 缓存

--- src/BD.Common/Net/Http/IImageHttpClientService.cs
+++ src/BD.Common/Net/Http/IImageHttpClientService.cs
@@ -1,48 +1,25 @@
+// ReSharper disable once CheckNamespace
 namespace System.Net.Http;
 
 public interface IImageHttpClientService
 {
     protected const string TAG = "ImageHttpClient";
 
+    protected const HttpHandlerCategory DefaultHttpHandlerCategory = HttpHandlerCategory.UserInitiated;
+
     /// <summary>
-    /// (带本地缓存)通过 Get 请求 Image Stream
+    /// 通过 Get 请求 Image <see cref="MemoryStream"/>
     /// </summary>
     /// <param name="requestUri"></param>
-    /// <param name="channelType">渠道类型，根据不同的类型建立不同的缓存文件夹</param>
-    /// <param name="cancellationToken"></param>
+    /// <param name="isPolly">是否使用 <see cref="Polly"/> 重试</param>
+    /// <param name="cache">是否使用缓存</param>
+    /// <param name="category">使用的调度器种类</param>
+    /// <param name="cancellationToken">取消操作</param>
     /// <returns></returns>
-    Task<Stream?> GetImageStreamAsync(
+    Task<MemoryStream?> GetImageMemoryStreamAsync(
         string requestUri,
-        string channelType,
+        bool isPolly = true,
+        bool cache = false,
+        HttpHandlerCategory category = DefaultHttpHandlerCategory,
         CancellationToken cancellationToken = default);
-
-    /// <summary>
-    /// (带本地缓存)通过 Get 请求 Image FilePath
-    /// </summary>
-    /// <param name="requestUri"></param>
-    /// <param name="channelType">渠道类型，根据不同的类型建立不同的缓存文件夹</param>
-    /// <param name="cancellationToken"></param>
-    /// <returns></returns>
-    Task<string?> GetImageAsync(
-        string requestUri,
-        string channelType,
-        CancellationToken cancellationToken = default);
-
-    /// <summary>
-    /// (无本地缓存)通过 Get 请求 Image 内容
-    /// </summary>
-    /// <param name="requestUri"></param>
-    /// <param name="cancellationToken"></param>
-    /// <returns></returns>
-    Task<Stream?> GetImageStreamAsync(string requestUri, CancellationToken cancellationToken = default);
-
-    static string GetImagesCacheDirectory(string? channelType)
-    {
-        const string dirName = "Images";
-        var dirPath = !string.IsNullOrWhiteSpace(channelType) ?
-            Path.Combine(IOPath.CacheDirectory, dirName, channelType) :
-            Path.Combine(IOPath.CacheDirectory, dirName);
-        IOPath.DirCreateByNotExists(dirPath);
-        return dirPath;
-    }
 }
--- src/BD.Common/Net/Http/ImageHttpClientService.cs
+++ src/BD.Common/Net/Http/ImageHttpClientService.cs
@@ -1,15 +1,119 @@
+using Polly;
 using static System.Net.Http.IImageHttpClientService;
 
+// ReSharper disable once CheckNamespace
 namespace System.Net.Http;
 
 public sealed partial class ImageHttpClientService : GeneralHttpClientFactory, IImageHttpClientService
 {
     public ImageHttpClientService(
-            ILoggerFactory loggerFactory,
-            IHttpPlatformHelperService http_helper,
-            IHttpClientFactory clientFactory)
-            : base(loggerFactory.CreateLogger(TAG), http_helper, clientFactory)
+        ILoggerFactory loggerFactory,
+        IHttpPlatformHelperService http_helper,
+        IHttpClientFactory clientFactory)
+        : base(loggerFactory.CreateLogger(TAG), http_helper, clientFactory)
     {
 
     }
+
+    public async Task<MemoryStream?> GetImageMemoryStreamAsync(
+        string requestUri,
+        bool isPolly = true,
+        bool cache = false,
+        HttpHandlerCategory category = DefaultHttpHandlerCategory,
+        CancellationToken cancellationToken = default)
+    {
+        if (!String2.IsHttpUrl(requestUri))
+            return default;
+
+        if (!cache)
+        {
+            category = HttpHandlerCategory.Default;
+        }
+        else
+        {
+            isPolly = false;
+        }
+
+        MemoryStream? response = null;
+        try
+        {
+            if (isPolly)
+            {
+                response = await Policy.HandleResult<MemoryStream?>(x => x == null)
+                    .WaitAndRetryAsync(numRetries, PollyRetryAttempt)
+                    .ExecuteAsync(_GetImageMemoryStreamCoreAsync, cancellationToken);
+            }
+            else
+            {
+                response = await _GetImageMemoryStreamCoreAsync(cancellationToken);
+            }
+        }
+        catch (Exception e)
+        {
+            logger.LogWarning(e,
+                $"{nameof(GetImageMemoryStreamAsync)} fail, category: {{category}}.", category);
+        }
+
+        if (response == null && category != HttpHandlerCategory.Offline && cache)
+        {
+            try
+            {
+                category = HttpHandlerCategory.Offline;
+                response = await _GetImageMemoryStreamCoreAsync(cancellationToken);
+            }
+            catch (Exception e)
+            {
+                logger.LogWarning(e,
+                    $"{nameof(GetImageMemoryStreamAsync)} fail, category: {{category}}.", category);
+            }
+        }
+
+        return response;
+
+        Task<MemoryStream?> _GetImageMemoryStreamCoreAsync(CancellationToken cancellationToken)
+            => GetImageMemoryStreamCoreAsync(
+                requestUri,
+                category,
+                cancellationToken);
+    }
+
+    async Task<MemoryStream?> GetImageMemoryStreamCoreAsync(
+        string requestUri,
+        HttpHandlerCategory category = DefaultHttpHandlerCategory,
+        CancellationToken cancellationToken = default)
+    {
+        var request = new HttpRequestMessage(HttpMethod.Get, requestUri);
+        request.Headers.Accept.ParseAdd(http_helper.AcceptImages);
+        request.Headers.UserAgent.ParseAdd(http_helper.UserAgent);
+        var client = CreateClient(TAG, category);
+        var response = await client.SendAsync(request,
+            HttpCompletionOption.ResponseHeadersRead,
+            cancellationToken)
+            .ConfigureAwait(false);
+        if (response.IsSuccessStatusCode)
+        {
+            var imageStream = await response.Content.ReadAsStreamAsync(cancellationToken);
+            var memoryStream = new MemoryStream();
+            await imageStream.CopyToAsync(memoryStream, cancellationToken);
+            memoryStream.Position = 0;
+            return memoryStream;
+        }
+
+        return default;
+    }
+
+    #region Polly
+
+    const int numRetries = 3;
+
+    static TimeSpan PollyRetryAttempt(int attemptNumber)
+    {
+        var powY = attemptNumber % numRetries;
+        var timeSpan = TimeSpan.FromMilliseconds(Math.Pow(2, powY));
+        int addS = attemptNumber / numRetries;
+        if (addS > 0) timeSpan = timeSpan.Add(TimeSpan.FromSeconds(addS));
+        return timeSpan;
+    }
+
+    #endregion
 }
