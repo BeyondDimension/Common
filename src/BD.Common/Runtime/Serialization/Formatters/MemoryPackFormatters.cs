1899f7d68c0c29665f10c457ff64647546db02bf
Aigio Liu <lysoft.pw@foxmail.com>
2023-06-29 16:22:35 +0800

    🔖 1.23.10629.11622

--- src/BD.Common/Runtime/Serialization/Formatters/MemoryPackFormatters.cs
+++ src/BD.Common/Runtime/Serialization/Formatters/MemoryPackFormatters.cs
@@ -2,24 +2,33 @@ namespace System.Runtime.Serialization.Formatters;
 
 public sealed class MemoryPackFormatters : IMemoryPackFormatterRegister
 {
+    public static Action<Type>? OnRegister { get; set; }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    static void Register<T>(MemoryPackFormatter<T> formatter)
+    {
+        MemoryPackFormatterProvider.Register(formatter);
+        OnRegister?.Invoke(typeof(T));
+    }
+
     static void IMemoryPackFormatterRegister.RegisterFormatter()
     {
-        MemoryPackFormatterProvider.Register(CookieFormatterAttribute.Formatter.Default);
-        MemoryPackFormatterProvider.Register(CookieCollectionFormatterAttribute.Formatter.Default);
-        MemoryPackFormatterProvider.Register(CookieContainerFormatterAttribute.Formatter.Default);
+        Register(CookieFormatterAttribute.Formatter.Default);
+        Register(CookieCollectionFormatterAttribute.Formatter.Default);
+        Register(CookieContainerFormatterAttribute.Formatter.Default);
 
-        MemoryPackFormatterProvider.Register(IPAddressFormatterAttribute.Formatter.Default);
+        Register(IPAddressFormatterAttribute.Formatter.Default);
 
-        MemoryPackFormatterProvider.Register(RSAParametersFormatterAttribute.Formatter.Default);
+        Register(RSAParametersFormatterAttribute.Formatter.Default);
 
-        MemoryPackFormatterProvider.Register(ColorFormatterAttribute.Formatter.Default);
-        MemoryPackFormatterProvider.Register(SplatColorFormatterAttribute.Formatter.Default);
-        MemoryPackFormatterProvider.Register(NullableColorFormatterAttribute.Formatter.Default);
-        MemoryPackFormatterProvider.Register(NullableSplatColorFormatterAttribute.Formatter.Default);
+        Register(ColorFormatterAttribute.Formatter.Default);
+        Register(SplatColorFormatterAttribute.Formatter.Default);
+        Register(NullableColorFormatterAttribute.Formatter.Default);
+        Register(NullableSplatColorFormatterAttribute.Formatter.Default);
 
-        MemoryPackFormatterProvider.Register(X509CertificateFormatterAttribute.Formatter.Default);
-        MemoryPackFormatterProvider.Register(X509Certificate2FormatterAttribute.Formatter.Default);
-        MemoryPackFormatterProvider.Register(X509CertificatePackableNullableFormatterAttribute.Formatter.Default);
-        MemoryPackFormatterProvider.Register(X509CertificatePackableFormatterAttribute.Formatter.Default);
+        Register(X509CertificateFormatterAttribute.Formatter.Default);
+        Register(X509Certificate2FormatterAttribute.Formatter.Default);
+        Register(X509CertificatePackableNullableFormatterAttribute.Formatter.Default);
+        Register(X509CertificatePackableFormatterAttribute.Formatter.Default);
     }
 }
