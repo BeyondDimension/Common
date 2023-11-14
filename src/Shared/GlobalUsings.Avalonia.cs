bbbffdb16a256accd43cc961c59204c876cd8f2f
Aigio Liu <lysoft.pw@foxmail.com>
2023-11-15 00:33:39 +0800

    🎨 global using Avalonia

--- src/Shared/GlobalUsings.Avalonia.cs
+++ src/Shared/GlobalUsings.Avalonia.cs
@@ -6,10 +6,20 @@
 #pragma warning disable SA1211 // Using alias directives should be ordered alphabetically by alias name
 
 global using Avalonia;
+global using Avalonia.Animation;
 global using Avalonia.Controls;
 global using Avalonia.Controls.Primitives;
-global using Avalonia.Media;
-global using Avalonia.Threading;
 global using Avalonia.Controls.ApplicationLifetimes;
+global using Avalonia.Media;
+global using Avalonia.Media.Imaging;
+global using Avalonia.Threading;
+global using Avalonia.Platform;
 global using Avalonia.Platform.Storage;
+global using Avalonia.Metadata;
+global using Avalonia.Rendering.Composition;
+global using Avalonia.VisualTree;
+global using Avalonia.Logging;
+global using AvaSize = Avalonia.Size;
+global using AvaPoint = Avalonia.Point;
+global using AvaVector = Avalonia.Vector;
 global using AvaApplication = Avalonia.Application;
