b4cd6a6013993bf33e38bca008920e8e5e0212bc
Aigio Liu <lysoft.pw@foxmail.com>
2024-04-12 16:10:36 +0800

    ✨ UseBMS

--- src/BD.Common8.AspNetCore.Identity.BackManage/Extensions/ServiceCollectionExtensions.AddBMS.cs
+++ src/BD.Common8.AspNetCore.Identity.BackManage/Extensions/ServiceCollectionExtensions.AddBMS.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Diagnostics;
+
 namespace Microsoft.Extensions.DependencyInjection;
 
 public static partial class ServiceCollectionExtensions
@@ -40,5 +42,65 @@ public static partial class ServiceCollectionExtensions
         builder.Services.AddBMRepositories<TContext>();
         builder.Services.AddBMIdentity<TBMAppSettings, TContext>(appSettings);
         builder.Services.AddBMWebApi(appSettings, configureApplicationPartManager);
+    }
+
+    /// <summary>
+    /// 使用后台管理系统
+    /// </summary>
+    /// <typeparam name="TBMAppSettings"></typeparam>
+    /// <param name="app"></param>
+    /// <param name="appSettings"></param>
+    public static void UseBMS<TBMAppSettings>(
+        this WebApplication app,
+        TBMAppSettings appSettings)
+        where TBMAppSettings : BMAppSettings
+    {
+        app.UseExceptionHandler(exceptionHandlerApp => // https://learn.microsoft.com/zh-cn/aspnet/core/fundamentals/error-handling
+        {
+            exceptionHandlerApp.Run(async context =>
+            {
+                context.Response.StatusCode = StatusCodes.Status200OK;
+                context.Response.ContentType = MediaTypeNames.JSON;
+                var exception = context.Features.Get<IExceptionHandlerPathFeature>()?.Error;
+                var rsp = new ApiResponse
+                {
+                    Messages = [exception?.ToString() ?? ""],
+                };
+                await context.Response.WriteAsJsonAsync(rsp);
+            });
+        });
+
+        app.UseResponseCompression();
+
+        if (!appSettings.NotUseForwardedHeaders)
+        {
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto,
+                RequireHeaderSymmetry = false,
+                ForwardLimit = null,
+                KnownProxies = { IPAddress.Parse(string.IsNullOrWhiteSpace(appSettings.ForwardedHeadersKnownProxies) ? "::ffff:172.18.0.1" : appSettings.ForwardedHeadersKnownProxies), },
+            });
+        }
+
+        if (!appSettings.DisabledApiDoc)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.MapGet("/", () => Results.Redirect("/swagger/index.html"));
+        }
+        else
+        {
+            app.UseWelcomePage("/");
+        }
+
+        if (appSettings.UseCors)
+        {
+            app.UseCors();
+        }
+
+        app.UseAuthentication(); // 鉴权，检测有没有登录，登录的是谁，赋值给 User
+        app.UseAuthorization(); // 授权，检测权限
+        app.MapControllers();
     }
 }
