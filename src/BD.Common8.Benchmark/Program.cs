3528ccba66989bd34364b1159b966ff4d29fe1c9
Aigio Liu <lysoft.pw@foxmail.com>
2024-06-14 15:19:30 +0800

    🎨 Benchmark

--- src/BD.Common8.Benchmark/Program.cs
+++ src/BD.Common8.Benchmark/Program.cs
@@ -1,4 +1,3 @@
-using BD.Common8.Models;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 using Microsoft.International.Converters.PinYinConverter;
@@ -7,7 +6,8 @@ using Microsoft.International.Converters.PinYinConverter;
 
 namespace BD.Common8.Benchmark;
 
-[InProcess]
+#pragma warning disable IDE1006 // 命名样式
+
 [BinaryResource(
 """
 [
@@ -21,8 +21,9 @@ public partial class BinaryResourceStreamTest
 {
     CharDictionary? dict1;
     CharDictionary? dict2;
+    CharDictionary? dict3;
 
-    [Benchmark]
+    [Benchmark(Baseline = true)]
     public unsafe void BinaryResource_Stream_SourceGenerator()
     {
         var bytes = _CharDictionary();
@@ -35,7 +36,7 @@ public partial class BinaryResourceStreamTest
         }
     }
 
-    [Benchmark(Baseline = true)]
+    [Benchmark]
     public void BinaryResource_Stream_ManifestResource()
     {
         Assembly executingAssembly = typeof(BinaryResourceStreamTest).Assembly;
@@ -43,9 +44,22 @@ public partial class BinaryResourceStreamTest
         using BinaryReader binaryReader = new BinaryReader(manifestResourceStream!);
         dict2 = CharDictionary.Deserialize(binaryReader);
     }
+
+    [Benchmark]
+    public unsafe void BinaryResource_Stream_SourceGenerator_Reverse()
+    {
+        var bytes = ReverseBinaryResource._CharDictionary();
+        bytes.AsSpan().Reverse();
+        fixed (byte* ptr = bytes)
+        {
+            using UnmanagedMemoryStream stream = new(ptr, bytes.Length);
+            using BinaryReader binaryReader = new BinaryReader(stream);
+            dict3 = CharDictionary.Deserialize(binaryReader);
+            new Span<byte>(ptr, bytes.Length).Clear();
+        }
+    }
 }
 
-[InProcess]
 [BinaryResource(
 """
 [
@@ -58,8 +72,9 @@ public partial class BinaryResourceMemoryPackTest
 {
     District[]? all1;
     District[]? all2;
+    District[]? all3;
 
-    [Benchmark]
+    [Benchmark(Baseline = true)]
     public void BinaryResource_MemoryPack_SourceGenerator()
     {
         Span<byte> bytes = AMapAdcodeCitycode20210406();
@@ -74,12 +89,50 @@ public partial class BinaryResourceMemoryPackTest
         }
     }
 
-    [Benchmark(Baseline = true)]
+    [Benchmark]
     public void BinaryResource_MemoryPack_ManifestResource()
     {
         var all2 = MemoryPackSerializer.Deserialize<District[]>(Properties.Resources.AMap_adcode_citycode_20210406);
         ArgumentNullException.ThrowIfNull(all2);
     }
+
+    [Benchmark]
+    public void BinaryResource_MemoryPack_SourceGenerator_Reverse()
+    {
+        Span<byte> bytes = ReverseBinaryResource.AMapAdcodeCitycode20210406();
+        bytes.Reverse();
+        try
+        {
+            all3 = MemoryPackSerializer.Deserialize<District[]>(bytes);
+            ArgumentNullException.ThrowIfNull(all3);
+        }
+        finally
+        {
+            bytes.Clear();
+        }
+    }
+}
+
+[BinaryResource(
+"""
+[
+  {
+    "Path": "..\\..\\res\\AMap_adcode_citycode_20210406",
+    "Name": "AMapAdcodeCitycode20210406_Reverse",
+    "Reverse": true
+  },
+  {
+    "Path": "..\\Microsoft Visual Studio International Pack\\Simplified Chinese Pin-Yin Conversion Library\\ChnCharInfo\\CharDictionary",
+    "Name": "_CharDictionary_Reverse",
+    "Reverse": true
+  }
+]
+""")]
+static partial class ReverseBinaryResource
+{
+    internal static byte[] AMapAdcodeCitycode20210406() => AMapAdcodeCitycode20210406_Reverse();
+
+    internal static byte[] _CharDictionary() => _CharDictionary_Reverse();
 }
 
 public class Program
