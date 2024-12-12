c67ff83da1dcce072fe2ac70e555a34dbab0fc70
lxy829 <3346435289@qq.com>
2024-12-12 18:23:39 +0800

    🎨 OSHelper

--- src/BD.Common8.Bcl/OSHelper.windows.cs
+++ src/BD.Common8.Bcl/OSHelper.windows.cs
@@ -1,3 +1,7 @@
+#if WINDOWS
+using Windows.ApplicationModel;
+#endif
+
 namespace System;
 
 partial class OSHelper
@@ -14,5 +18,47 @@ partial class OSHelper
         _IsWindows11AtLeast.Value;
 
     static readonly Lazy<bool> _IsWindows11AtLeast = new(() => OperatingSystem.IsWindowsVersionAtLeast(10, 0, 22000));
+#endif
+
+#if WINDOWS
+    static string? GetAppUserModelId()
+    {
+        try
+        {
+            // 当由未打包的应用程序使用时，访问此属性会引发 InvalidOperationException 和 HRESULT，指示“进程没有包标识”。80073D54
+#pragma warning disable CA1416 // 验证平台兼容性
+            var appInfo = AppInfo.Current;
+#pragma warning restore CA1416 // 验证平台兼容性
+            if (appInfo != null)
+            {
+                return appInfo.AppUserModelId;
+            }
+        }
+        catch
+        {
+            try
+            {
+                return $"{Package.Current.Id.FamilyName}!App";
+            }
+            catch
+            {
+            }
+        }
+        return null;
+    }
+
+    const string Shell_AppsFolder_ = @"Shell:AppsFolder\{0}";
+
+    /// <summary>
+    /// 获取 UWP 应用程序的 Shell:AppsFolder 路径，例如：Shell:AppsFolder\Microsoft.WindowsCalculator_8wekyb3d8bbwe!App
+    /// <para>返回 <see langword="null"/> 时表示获取当前应用程序的 AppUserModelId 失败，当前应用程序缺少表标识</para>
+    /// </summary>
+    /// <param name="appUserModelId">传递 <see langword="null"/> 时将使用当前应用程序的 AppUserModelId</param>
+    /// <returns></returns>
+    public static string? GetShellAppsFolderPath(string? appUserModelId = null)
+    {
+        appUserModelId ??= GetAppUserModelId();
+        return appUserModelId == null ? null : string.Format(Shell_AppsFolder_, appUserModelId);
+    }
 #endif
 }
