e24d3d4bf3be5224424ac86711039647a878d6d8
Aigio Liu <lysoft.pw@foxmail.com>
2025-03-14 08:23:24 +0800

    🎨 Match char Span

--- src/BD.Common8.Bcl/Extensions/MatchExtensions.cs
+++ src/BD.Common8.Bcl/Extensions/MatchExtensions.cs
@@ -28,4 +28,64 @@ public static partial class MatchExtensions
             if (action.Invoke(item))
                 yield return item.Value.Trim();
     }
+
+    /// <summary>
+    /// 获取正则表达式匹配的单个字符串值
+    /// </summary>
+    /// <param name="regex"></param>
+    /// <param name="input"></param>
+    /// <param name="defaultValue"></param>
+    /// <returns></returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    [return: NotNullIfNotNull(nameof(defaultValue))]
+    public static string? GetValue(
+        this Regex regex,
+        ReadOnlySpan<char> input,
+        string? defaultValue = "")
+    {
+#if DEBUG
+        var a = regex.Match(new string(input));
+        var b = a.GetValue(it => it.Success);
+#endif
+
+        foreach (var it in regex.EnumerateMatches(input))
+        {
+            if (input.IsEmpty)
+            {
+                break;
+            }
+            var val = input.Slice(it.Index, it.Length).Trim();
+            if (val.EndsWith(Environment.NewLine))
+            {
+                val = val[..^Environment.NewLine.Length];
+            }
+            val = val.Trim();
+            return new(val);
+        }
+        return defaultValue;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    [return: NotNullIfNotNull(nameof(defaultValue))]
+    public static string? GetJoinValue(
+        this Regex regex,
+        ReadOnlySpan<char> input,
+        char separator,
+        string? defaultValue = "")
+    {
+        List<string> list = new();
+        foreach (var it in regex.EnumerateMatches(input))
+        {
+            if (input.IsEmpty)
+            {
+                break;
+            }
+            list.Add(new(input.Slice(it.Index, it.Length)));
+        }
+        if (list.Count != 0)
+        {
+            return string.Join(separator, list);
+        }
+        return defaultValue;
+    }
 }
